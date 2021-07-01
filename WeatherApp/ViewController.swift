8667eeb3351ceb79e1b3cdf69b47eca6263eb492
mine rala <mine-rala@hotmail.com>
2021-07-01 13:49:10 +0300

    write mine

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    //ndkjsandaskjddm
+    var name:String = ""
+   
+    
+    var mine : String = ""
 }
 
