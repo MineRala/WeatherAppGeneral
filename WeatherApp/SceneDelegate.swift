1db649c68cd8b9e754e5ad1838b58aa717e2d684
aybek can kaya <aybek.kaya@rapsodo.com>
2021-07-05 23:42:59 +0300

    ipad storyboard added

--- WeatherApp/SceneDelegate.swift
+++ WeatherApp/SceneDelegate.swift
@@ -16,7 +16,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let ws = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: ws)
+        let storyboardName = UIDevice.current.userInterfaceIdiom == .pad ? "MainIpad" : "Main"
+        let storyboard = UIStoryboard(name: storyboardName, bundle: nil)
+        let vc = storyboard.instantiateInitialViewController()
+        self.window?.rootViewController = vc
+        self.window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
